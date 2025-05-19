a9bfcd13fbcff3f26723df6857858f63e48d40f0
Joel Chi <abongsjoel@gmail.com>
2025-05-19 10:20:32 +0100

    Add assign username route

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const Message = require("../models/Message");
+const User = require("../models/User");
 
 const router = express.Router();
 
@@ -9,6 +10,54 @@ const router = express.Router();
 router.get("/users", async (req, res) => {
   const users = await Message.distinct("userId");
   res.json(users);
+});
+
+// POST /api/users/assign-name
+router.post("/users/assign-name", async (req, res) => {
+  const { username, userId } = req.body;
+
+  if (!username || !userId) {
+    return res
+      .status(400)
+      .json({ error: "Both username and phone are required" });
+  }
+
+  try {
+    const existing = await User.findOneAndUpdate(
+      { userId },
+      { username },
+      { upsert: true, new: true, setDefaultsOnInsert: true }
+    );
+
+    res.status(200).json({ success: true, user: existing });
+  } catch (err) {
+    console.error("Error assigning username:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// POST /api/users/assign-name
+router.post("/users/assign-name", async (req, res) => {
+  const { username, phone } = req.body;
+
+  if (!username || !phone) {
+    return res
+      .status(400)
+      .json({ error: "Both username and phone are required" });
+  }
+
+  try {
+    const existing = await User.findOneAndUpdate(
+      { phone },
+      { username },
+      { upsert: true, new: true, setDefaultsOnInsert: true }
+    );
+
+    res.status(200).json({ success: true, user: existing });
+  } catch (err) {
+    console.error("Error assigning username:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 router.get("/messages/:userId", async (req, res) => {
