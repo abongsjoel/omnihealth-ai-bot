1d920787480aeacdc6fac325151bbba5e91dcada
Joel Chi <abongsjoel@gmail.com>
2025-05-19 11:18:22 +0100

    File adjustments

--- routes/messages.js
+++ routes/messages.js
@@ -4,6 +4,12 @@ const axios = require("axios");
 const Message = require("../models/Message");
 const router = express.Router();
 
+// GET /api/users
+router.get("/users", async (req, res) => {
+  const users = await Message.distinct("userId");
+  res.json(users);
+});
+
 router.get("/messages/:userId", async (req, res) => {
   const messages = await Message.find({ userId: req.params.userId })
     .sort({ timestamp: 1 })
--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,16 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-// GET /api/users
-router.get("/users", async (req, res) => {
-  const users = await Message.distinct("userId");
-  res.json(users);
-});
-
 // POST /api/users/assign-name
 router.post("/users/assign-name", async (req, res) => {
   const { username, userId } = req.body;
 
+  console.log("Assigning name:", { username, userId, reqbody: req.body });
+
   if (!username || !userId) {
     return res
       .status(400)
-      .json({ error: "Both username and phone are required" });
+      .json({ error: "Both username and userId are required TJ" });
   }
 
   try {
